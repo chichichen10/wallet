f944341dbaf2584d43cdfdabae762cb1a9e6ffd5
CHEN Ying Chi <chichichen10@gmail.com>
2021-09-02 15:36:52 +0800

    fix

--- App.tsx
+++ App.tsx
@@ -1,24 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import TabOneScreen from './screens/TabOneScreen';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return ( 
-      <SafeAreaProvider>
-       
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-        
-      </SafeAreaProvider>
-    );
   }
+  return (
+    <SafeAreaProvider>
+      <TabOneScreen />
+    </SafeAreaProvider>
+  );
 }
--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { Text, TextProps } from './Themed';
 
-export function MonoText(props: TextProps) {
+export default function MonoText(props: TextProps) {
+  // eslint-disable-next-line react/destructuring-assignment
   return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
 }
